f5bae0078166c0af015f989fef28e2e554d36dd5
oldkaseb <hamedsocialworks@gmail.com>
2025-09-18 16:38:12 +0400

    Update main.py

--- main.py
+++ main.py
@@ -800,6 +800,7 @@ async def on_checksub_group(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # ---------- دریافت متن نجوا در خصوصی ----------
 async def private_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
     if update.effective_chat.type != ChatType.PRIVATE:
         return
 
